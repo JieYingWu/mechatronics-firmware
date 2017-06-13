f75948e7fc037005aca4640a69b657b52cccc953
JieYing <jieying@jhu.edu>
2017-06-13 00:12:33 -0700

    Simplified mux logic in velocity estimation

--- FPGA1394_QLA/Verilog/EncPeriod.v
+++ FPGA1394_QLA/Verilog/EncPeriod.v
@@ -21,12 +21,13 @@
 
 // ---------- Peter ------------------
 module EncPeriod(
-   input wire clk_fast,     // count this clock between encoder ticks
-   input wire reset,        // global reset signal
-   input wire ticks,        // encoder transition signal
-   input wire dir,          // direction of the ticks
-   output wire ticks_en,    // edge signal 
-   output reg[31:0] count  // number clk_fast periods per tick
+   input wire clk_fast,      // count this clock between encoder ticks
+   input wire reset,         // global reset signal
+   input wire ticks,         // encoder transition signal
+   input wire dir,           // direction of the ticks
+   output wire ticks_en,     // edge signal 
+   output wire[31:0] latched, // latched counter from last encoder event
+   output wire[31:0] count    // number clk_fast periods per tick
 );
 
     // local registers 
@@ -47,19 +48,8 @@ reg dir_changed; //changed direction in this cycle
 reg ticks_r;    // previous ticks
 assign ticks_en = ticks & (~ticks_r);
 
-// choose output to be the larger of latched value or free-running counter
-always @(posedge clk_fast) 
-begin
-   ticks_r <= ticks;
-	dir_r <= dir;
-   
-	if (cnter >= cnter_latch) begin
-		count <= {0, dir, dir_changed, 7'h00, cnter};
-	end 
-	else begin
-		count <= {1, dir, dir_changed, 7'h00, cnter_latch};
-	end
-end
+assign latched = {1, dir, dir_changed, 7'h00, cnter_latch};
+assign count = {0, dir, dir_changed, 7'h00, cnter};
 
 // latch cnter value 
 always @(posedge ticks_en or negedge reset)
@@ -109,18 +99,22 @@ module EncPeriodQuad(
     wire a_dn_tick;
     wire b_up_tick;
     wire b_dn_tick;
-    wire[31:0] perd_a_up;   // channel a up data 
-    wire[31:0] perd_a_dn;   // channel a dn data
-    wire[31:0] perd_b_up;   // channel b up data
-    wire[31:0] perd_b_dn;   // channel b dn data
+    wire[31:0] a_up_latched;   // channel a up data 
+    wire[31:0] a_dn_latched;   // channel a dn data
+    wire[31:0] b_up_latched;   // channel b up data
+    wire[31:0] b_dn_latched;   // channel b dn latched_value
+    wire[31:0] a_up_counter;   // channel a up free running counter
+    wire[31:0] a_dn_counter;   // channel a dn free running counter
+    wire[31:0] b_up_counter;   // channel b up free running counter
+    wire[31:0] b_dn_counter;   // channel b dn free running counter
 
 //------------------------------------------------------------------------------
 // hardware description
 //
-EncPeriod EncPerUpA(clk_fast, reset,  a, dir, a_up_tick, perd_a_up);
-EncPeriod EncPerDnA(clk_fast, reset, ~a, dir, a_dn_tick, perd_a_dn);
-EncPeriod EncPerUpB(clk_fast, reset,  b, dir, b_up_tick, perd_b_up);
-EncPeriod EncPerDnB(clk_fast, reset, ~b, dir, b_dn_tick, perd_b_dn);
+EncPeriod EncPerUpA(clk_fast, reset,  a, dir, a_up_tick, a_up_latched, a_up_counter);
+EncPeriod EncPerDnA(clk_fast, reset, ~a, dir, a_dn_tick, a_dn_latched, a_dn_counter);
+EncPeriod EncPerUpB(clk_fast, reset,  b, dir, b_up_tick, b_up_latched, b_up_counter);
+EncPeriod EncPerDnB(clk_fast, reset, ~b, dir, b_dn_tick, b_dn_latched, b_dn_counter);
 
 localparam[1:0] a_up = 2'b00;
 localparam[1:0] a_dn = 2'b01;
@@ -154,50 +148,50 @@ always @(posedge clk_fast or negedge reset) begin
    end
    
    else if (mux == a_up) begin  // A up
-      if ((dir == 0) && (~perd_b_up[31]) && (~perd_b_up[29]) && (perd_b_up[21:0] > perd_a_up[21:0])) begin
-         period <= {perd_b_up[31:29], b_up, 2'b01, perd_b_up[24:0]};
+      if ((dir == 0) && (~b_up_counter[29]) && (b_up_counter[21:0] > a_up_latched[21:0])) begin
+         period <= {b_up_counter[31:29], b_up, 2'b01, b_up_counter[24:0]};
       end 
-      else if ((dir == 1) && (~perd_b_dn[31]) && (~perd_b_dn[29]) && (perd_b_dn[21:0] > perd_a_up[21:0])) begin
-         period <= {perd_b_dn[31:29], b_dn, 2'b10, perd_b_dn[24:0]};
+      else if ((dir == 1) && (~b_dn_counter[29]) && (b_dn_counter[21:0] > a_up_latched[21:0])) begin
+         period <= {b_dn_counter[31:29], b_dn, 2'b10, b_dn_counter[24:0]};
       end 
       else begin
-         period <= {perd_a_up[31:29], a_up, 2'b11, perd_a_up[24:0]};
+         period <= {a_up_latched[31:29], a_up, 2'b11, a_up_latched[24:0]};
       end
    end
    
    else if (mux == b_up) begin  // B up
-      if ((dir == 0) && (~perd_a_dn[31]) && (~perd_a_dn[29]) && (perd_a_dn[21:0] > perd_b_up[21:0])) begin
-         period <= {perd_a_dn[31:29], a_dn, 2'b01, perd_a_dn[24:0]};
+      if ((dir == 0) && (~a_dn_latched[29]) && (a_dn_counter[21:0] > b_up_latched[21:0])) begin
+         period <= {a_dn_counter[31:29], a_dn, 2'b01, a_dn_counter[24:0]};
       end 
-      else if ((dir == 1) && (~perd_a_up[31]) && (~perd_a_up[29] )&& (perd_a_up[21:0] > perd_b_up[21:0])) begin
-         period <= {perd_a_up[31:29], a_up, 2'b10, perd_a_up[24:0]};
+      else if ((dir == 1) && (~a_up_counter[29] )&& (a_up_counter[21:0] > b_up_latched[21:0])) begin
+         period <= {a_up_counter[31:29], a_up, 2'b10, a_up_counter[24:0]};
       end 
       else begin
-         period <= {perd_b_up[31:29], b_up, 2'b11, perd_b_up[24:0]};
+         period <= {b_up_latched[31:29], b_up, 2'b11, b_up_latched[24:0]};
       end
    end
    
    else if (mux == a_dn) begin  // A down
-      if ((dir == 0) && (~perd_b_dn[31]) && (~perd_b_dn[29]) && (perd_b_dn[21:0] > perd_a_dn[21:0])) begin
-         period <= {perd_b_dn[31:29], b_dn, 2'b01, perd_b_dn[24:0]};
+      if ((dir == 0) && (~b_dn_latched[29]) && (b_dn_counter[21:0] > a_dn_latched[21:0])) begin
+         period <= {b_dn_counter[31:29], b_dn, 2'b01, b_dn_counter[24:0]};
       end 
-      else if ((dir == 1) && (~perd_b_up[31]) && (~perd_b_up[29]) && (perd_b_up[21:0] > perd_a_dn[21:0])) begin
-         period <= {perd_b_up[31:29], b_up, 2'b10, perd_b_up[24:0]};
+      else if ((dir == 1) && (~b_up_counter[29]) && (b_up_counter[21:0] > a_dn_latched[21:0])) begin
+         period <= {b_up_counter[31:29], b_up, 2'b10, b_up_counter[24:0]};
       end 
       else begin
-         period <= {perd_a_dn[31:29], a_dn, 2'b11, perd_a_dn[24:0]};
+         period <= {a_dn_counter[31:29], a_dn, 2'b11, a_dn_counter[24:0]};
       end
    end
    
    else if (mux == b_dn) begin  // B down
-      if ((dir == 0) && (~perd_a_up[31]) && (~perd_a_up[29]) && (perd_a_up[21:0] > perd_b_dn[21:0])) begin
-         period <= {perd_a_up[31:29], a_up, 2'b01, perd_a_up[24:0]};
+      if ((dir == 0) && (~a_up_counter[29]) && (a_up_counter[21:0] > b_dn_latched[21:0])) begin
+         period <= {a_up_counter[31:29], a_up, 2'b01, a_up_counter[24:0]};
       end 
-      else if ((dir == 1) && (~perd_a_dn[31]) && (~perd_a_dn[29]) && (perd_a_dn[21:0] > perd_b_dn[21:0])) begin
-         period <= {perd_a_dn[31:29], a_dn, 2'b10, perd_a_dn[24:0]};
+      else if ((dir == 1) && (~a_dn_counter[29]) && (a_dn_counter[21:0] > b_dn_latched[21:0])) begin
+         period <= {a_dn_counter[31:29], a_dn, 2'b10, a_dn_counter[24:0]};
       end
       else begin
-         period <= {perd_b_dn[31:29], b_dn, 2'b11, perd_b_dn[24:0]};
+         period <= {b_dn_latched[31:29], b_dn, 2'b11, b_dn_latched[24:0]};
       end
    end
 end
